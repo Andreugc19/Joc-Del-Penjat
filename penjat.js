58cf053bf19e8da27c7603b84bfd2087a0d1a598
Andreugc19 <agarcia20507@iesjoanramis.org>
2023-11-28 17:19:44 +0100

    Dia 2: Joc del Penjat Andreu Garcia

---
+++ penjat.js
@@ -0,0 +1,37 @@
+// Capturar elements del DOM
+const contenidor = document.querySelector('.contenidor');
+const lletres = document.querySelectorAll('.lletra:not(.incorrecta, .correcta)');
+const contador = document.getElementById('contador');
+
+ompleUI();
+
+/* FUNCIONS */
+function actualitzaLletra() {
+    const lletresSeleccionades = document.querySelectorAll('.teclat .lletra');
+
+    const lletraIndex = [...lletresSeleccionades].map((lletra) => [...lletres].indexOf(lletra));
+
+    localStorage.setItem('lletresSeleccionades', JSON.stringify(lletraIndex));
+}
+
+function ompleUI() {
+    const lletresSeleccionades = JSON.parse(localStorage.getItem('lletresSeleccionades'));
+
+    if(lletresSeleccionades !== null && lletresSeleccionades.length > 0) {
+        lletres.forEach((lletra, index) => {
+            if(lletresSeleccionades.indexOf(index) > 1) {
+                lletra.classList.add('lletra');
+            }
+        });
+    }
+}
+
+contenidor.addEventListener('click', (e) => {
+    if(e.target.classList.contains('correcte') 
+        && !e.target.classList.contains('incorrecta')) {
+        e.target.classList.toggle('lletra');
+        actualitzaLletra();
+    }
+});
+
+actualitzaLletra();
